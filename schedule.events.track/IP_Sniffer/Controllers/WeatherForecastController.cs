34acbe69a4d393c589634db382b8f78ab85e5f83
Berthelmaster <berthel0802@gmail.com>
2021-03-22 22:40:31 +0100

    added tracker module

---
+++ schedule.events.track/IP_Sniffer/Controllers/WeatherForecastController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace IP_Sniffer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public Task<string> Get(bool allowForwarded = true)
+        {
+            if (allowForwarded)
+            {
+                // if you are allowing these forward headers, please ensure you are restricting context.Connection.RemoteIpAddress
+                // to cloud flare ips: https://www.cloudflare.com/ips/
+                string header = (HttpContext.Request.Headers["CF-Connecting-IP"].FirstOrDefault() ?? HttpContext.Request.Headers["X-Forwarded-For"].FirstOrDefault());
+                if (IPAddress.TryParse(header, out IPAddress ip))
+                {
+                    return Task.FromResult(ip.ToString());
+                }
+            }
+            return Task.FromResult(HttpContext.Connection.RemoteIpAddress.ToString());
+        }
+
+        [HttpPost]
+        public Task<HttpStatusCode> Post(string ip)
+        {
+
+        }
+    }
+}
