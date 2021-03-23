fb71e9e1d2a12ad990b74cc8a0c6cfc04e7cfde3
Berthelmaster <berthel0802@gmail.com>
2021-03-23 12:47:38 +0100

    added storage and changed layout

--- schedule.events.frontend/src/layouts/MainLayout.vue
+++ schedule.events.frontend/src/layouts/MainLayout.vue
@@ -10,46 +10,79 @@
           aria-label="Menu"
           @click="leftDrawerOpen = !leftDrawerOpen"
         />
-
-        <q-toolbar-title>
-          <div>Schedule Events</div>
-        </q-toolbar-title>
-
-        <div>Beta version v0.10</div>
-      </q-toolbar>
+        </q-toolbar>
+        <div class="q-px-lg q-pt-xl q-mb-md">
+          <q-toolbar-title>
+            <div class="row">
+              <div>Schedule Events</div>
+              <q-space />
+              <q-btn class="q-mr-sm" color="secondary" icon-right="mail" label="Login" />
+              <q-btn class="q-ml-sm" color="secondary" icon-right="mail" label="Register" />
+            </div>
+          </q-toolbar-title>
+        </div>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      content-class="bg-grey-1"
-    >
-      <q-list padding>
+      <q-drawer
+        v-model="leftDrawerOpen"
+        show-if-above
+        :width="200"
+        :breakpoint="400"
+      >
+        <q-scroll-area style="height: calc(100% - 150px); margin-top: 150px; border-right: 1px solid #ddd">
+          <q-list padding>
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="inbox" />
+              </q-item-section>
 
-        <q-item clickable v-ripple>
-          <q-item-section avatar>
-            <q-icon name="list" />
-          </q-item-section>
-          <q-item-section >
-            Todo
-          </q-item-section>
-        </q-item>
+              <q-item-section>
+                Inbox
+              </q-item-section>
+            </q-item>
 
-        <q-item clickable v-ripple>
-          <q-item-section avatar>
-            <q-icon name="help" />
-          </q-item-section>
-          <q-item-section >
-            Help
-          </q-item-section>
-        </q-item>
+            <q-item active clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="star" />
+              </q-item-section>
 
-        <div v-if="this.publicIp != null" class="fixed-bottom-left non-selectable">
-          <q-item-section>{{ this.publicIp }}</q-item-section>
-        </div>
-      </q-list>
-    </q-drawer>
+              <q-item-section>
+                Star
+              </q-item-section>
+            </q-item>
+
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="send" />
+              </q-item-section>
+
+              <q-item-section>
+                Send
+              </q-item-section>
+            </q-item>
+
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="drafts" />
+              </q-item-section>
+
+              <q-item-section>
+                Drafts
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-scroll-area>
+
+        <q-img class="absolute-top" src="https://cdn.quasar.dev/img/material.png" style="height: 150px">
+          <div class="absolute-bottom bg-transparent">
+            <q-avatar size="56px" class="q-mb-sm">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+            </q-avatar>
+            <div class="text-weight-bold">Razvan Stoenescu</div>
+            <div>@rstoenescu</div>
+          </div>
+        </q-img>
+      </q-drawer>
 
     <q-page-container>
       <keep-alive>
