3aca805acde0b03e77f4bdc8a7ed6f29e4e909a3
Berthelmaster <berthel0802@gmail.com>
2021-03-24 21:24:55 +0100

    added https

--- schedule.events.frontend/src/services/api.service.js
+++ schedule.events.frontend/src/services/api.service.js
@@ -21,7 +21,7 @@ const ApiService = {
     },
     getLocation(ip) {
         return new Promise(async (resolve, reject) => {
-            await axios.get(`http://ip-api.com/json/${ip}`)
+            await axios.get(`https://ip-api.com/json/${ip}`)
             .then(res => {
                 console.log(res.data.status)
                 if(res.data.status == 'success'){
