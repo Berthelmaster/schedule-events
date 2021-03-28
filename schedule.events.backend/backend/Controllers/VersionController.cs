9c084adb523194627d30f5fcc2d2994724b36508
Berthelmaster <berthel0802@gmail.com>
2021-03-28 12:49:51 +0200

    frontned version controller

---
+++ schedule.events.backend/backend/Controllers/VersionController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VersionController
+    {
+        private static string FrontendVersion => "1";
+
+        [HttpGet]
+        public Task<string> Get()
+        {
+            return Task.FromResult(FrontendVersion);
+        }
+    }
+}
