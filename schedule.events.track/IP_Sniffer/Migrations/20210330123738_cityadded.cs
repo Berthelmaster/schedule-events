ae52c63aef000f2e1db4cb6e3f8582c5442646da
Berthelmaster <berthel0802@gmail.com>
2021-03-30 14:38:09 +0200

    city added to tracker

---
+++ schedule.events.track/IP_Sniffer/Migrations/20210330123738_cityadded.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IP_Sniffer.Migrations
+{
+    public partial class cityadded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "GeoLocations",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "GeoLocations");
+        }
+    }
+}
--- schedule.events.track/IP_Sniffer/Models/GeoLocation.cs
+++ schedule.events.track/IP_Sniffer/Models/GeoLocation.cs
@@ -15,6 +15,7 @@ namespace IP_Sniffer.Models
         public int Id { get; set; }
         public string Ip { get; set; }
         public string Country { get; set; }
+        public string City { get; set; }
         public string Country_name { get; set; }
         public string Postal { get; set; }
         public string Region { get; set; }
