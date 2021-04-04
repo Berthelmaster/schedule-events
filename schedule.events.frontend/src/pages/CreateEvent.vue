0ef6ba48b234b3ae6c4f406b41dd45336b03061a
Berthelmaster <berthel0802@gmail.com>
2021-04-05 00:34:21 +0200

    updated create event

--- schedule.events.frontend/src/pages/CreateEvent.vue
+++ schedule.events.frontend/src/pages/CreateEvent.vue
@@ -1,28 +1,22 @@
 <template>
-    <q-page class="window-width row justify-center items-center bg-grey-3" style="padding: 5px;">
-        <div class="column relative-position mobile-width">
+    <q-page class="window-width row justify-center items-center bg-grey-3" style="">
+        <div>
         <div class="row">
-            <h5 class="text-h5 text-black q-my-md"><b>Create Event</b></h5>
+            <h5 class="text-h5 q-my-md"><b>Create Event</b></h5>
         </div>
         <div class="row">
             <q-card square bordered class="q-pa-lg shadow-1">
-            <q-card-section>
+            <q-card-section v-bind:class="{ activeDesktop: isDesktop}">
                 <q-form class="q-gutter-md">
-                    <div class="row">
-                    <div class="column">
-                        <q-input class="input-spacer-padding" square filled clearable v-model="email" type="email" label="email"/>
-                        <q-input class="input-spacer-padding" square filled clearable v-model="password" type="password" label="password" />
-                        <q-input class="input-spacer-padding" square filled clearable v-model="password" type="password" label="password" />
-                        <q-input class="input-spacer-padding" square filled clearable v-model="password" type="password" label="password" />
-                    </div>
-                        <div class="column">
-                        <q-input class="input-spacer-padding" square filled clearable v-model="email" type="email" label="email" />
-                        <q-input class="input-spacer-padding" square filled clearable v-model="password" type="password" label="password" />
-                        <q-input class="input-spacer-padding" square filled clearable v-model="password" type="password" label="password" />
-                        <q-input class="input-spacer-padding" square filled clearable v-model="password" type="password" label="password" />
-                    </div>
-                    </div>
-                    <q-editor v-model="editor" min-height="5rem" />
+                <q-input square filled clearable v-model="Event_Name" type="text" label="Event Name" />
+                <q-input square filled clearable v-model="Description" type="text" label="Description" />
+                <q-select square filled outlined v-model="selected_country" @input="v => { onCountryChanged(v) }" :options="countryList" label="Select country" />
+                <div v-if="cityList.length > 0">
+                    <q-select square filled outlined v-model="selected_city" @input="v => { onCityChanged(v) }" :options="cityList" label="Select city"/>
+                </div>
+                <div>
+                    <q-editor v-model="content" min-height="8rem" />
+                </div>
                 </q-form>
             </q-card-section>
             <q-card-actions class="q-px-md">
@@ -35,18 +29,288 @@
 </template>
 
 <script>
+import ApiService from '../services/api.service';
+import { Platform } from 'quasar'
 
 export default {
   name: 'PageIndex',
 
   data () {
     return {
-        email: '',
-        password: '',
-        editor: 'Write your event description here'
+        Event_Name: '',
+        Description: '',
+        content: 'Write your event description here',
+              selected_country: 'default',
+      countryList: [
+        "Afghanistan",
+        "Albania",
+        "Algeria",
+        "Andorra",
+        "Angola",
+        "Anguilla",
+        "Antarctica",
+        "Antigua and Barbuda",
+        "Argentina",
+        "Armenia",
+        "Aruba",
+        "Australia",
+        "Austria",
+        "Azerbaijan",
+        "Bahamas (the)",
+        "Bahrain",
+        "Bangladesh",
+        "Barbados",
+        "Belarus",
+        "Belgium",
+        "Belize",
+        "Benin",
+        "Bermuda",
+        "Bhutan",
+        "Bolivia",
+        "Bonaire, Sint Eustatius and Saba",
+        "Bosnia and Herzegovina",
+        "Botswana",
+        "Bouvet Island",
+        "Brazil",
+        "British Indian Ocean Territory",
+        "Brunei Darussalam",
+        "Bulgaria",
+        "Burkina Faso",
+        "Burundi",
+        "Cabo Verde",
+        "Cambodia",
+        "Cameroon",
+        "Canada",
+        "Cayman Islands",
+        "Central African Republic",
+        "Chad",
+        "Chile",
+        "China",
+        "Christmas Island",
+        "Colombia",
+        "Comoros",
+        "Congo",
+        "Cook Islands",
+        "Costa Rica",
+        "Croatia",
+        "Cuba",
+        "Curaçao",
+        "Cyprus",
+        "Czechia",
+        "Côte d'Ivoire",
+        "Denmark",
+        "Djibouti",
+        "Dominica",
+        "Dominican Republic",
+        "Ecuador",
+        "Egypt",
+        "El Salvador",
+        "Equatorial Guinea",
+        "Eritrea",
+        "Estonia",
+        "Eswatini",
+        "Ethiopia",
+        "Falkland Islands",
+        "Faroe Islands",
+        "Fiji",
+        "Finland",
+        "France",
+        "French Guiana",
+        "French Polynesia",
+        "French Southern Territories",
+        "Gabon",
+        "Gambia",
+        "Georgia",
+        "Germany",
+        "Ghana",
+        "Gibraltar",
+        "Greece",
+        "Greenland",
+        "Grenada",
+        "Guadeloupe",
+        "Guam",
+        "Guatemala",
+        "Guernsey",
+        "Guinea",
+        "Guinea-Bissau",
+        "Guyana",
+        "Haiti",
+        "Heard Island and McDonald Islands",
+        "Holy See",
+        "Honduras",
+        "Hong Kong",
+        "Hungary",
+        "Iceland",
+        "India",
+        "Indonesia",
+        "Iran",
+        "Iraq",
+        "Ireland",
+        "Isle of Man",
+        "Israel",
+        "Italy",
+        "Jamaica",
+        "Japan",
+        "Jersey",
+        "Jordan",
+        "Kazakhstan",
+        "Kenya",
+        "Kiribati",
+        "Korea",
+        "Kuwait",
+        "Kyrgyzstan",
+        "Lao People's Democratic Republic",
+        "Latvia",
+        "Lebanon",
+        "Lesotho",
+        "Liberia",
+        "Libya",
+        "Liechtenstein",
+        "Lithuania",
+        "Luxembourg",
+        "Macao",
+        "Madagascar",
+        "Malawi",
+        "Malaysia",
+        "Maldives",
+        "Mali",
+        "Malta",
+        "Marshall Islands",
+        "Martinique",
+        "Mauritania",
+        "Mauritius",
+        "Mayotte",
+        "Mexico",
+        "Micronesia",
+        "Moldova",
+        "Monaco",
+        "Mongolia",
+        "Montenegro",
+        "Montserrat",
+        "Morocco",
+        "Mozambique",
+        "Myanmar",
+        "Namibia",
+        "Nauru",
+        "Nepal",
+        "Netherlands",
+        "New Caledonia",
+        "New Zealand",
+        "Nicaragua",
+        "Niger",
+        "Nigeria",
+        "Niue",
+        "Norfolk Island",
+        "Northern Mariana Islands",
+        "Norway",
+        "Oman",
+        "Pakistan",
+        "Palau",
+        "Palestine, State of",
+        "Panama",
+        "Papua New Guinea",
+        "Paraguay",
+        "Peru",
+        "Philippines",
+        "Pitcairn",
+        "Poland",
+        "Portugal",
+        "Puerto Rico",
+        "Qatar",
+        "Republic of North Macedonia",
+        "Romania",
+        "Russian Federation",
+        "Rwanda",
+        "Réunion",
+        "Saint Barthélemy",
+        "Saint Helena, Ascension and Tristan da Cunha",
+        "Saint Kitts and Nevis",
+        "Saint Lucia",
+        "Saint Martin",
+        "Saint Pierre and Miquelon",
+        "Saint Vincent and the Grenadines",
+        "Samoa",
+        "San Marino",
+        "Sao Tome and Principe",
+        "Saudi Arabia",
+        "Senegal",
+        "Serbia",
+        "Seychelles",
+        "Sierra Leone",
+        "Singapore",
+        "Sint Maarten",
+        "Slovakia",
+        "Slovenia",
+        "Solomon Islands",
+        "Somalia",
+        "South Africa",
+        "South Georgia and the South Sandwich Islands",
+        "South Sudan",
+        "Spain",
+        "Sri Lanka",
+        "Sudan",
+        "Suriname",
+        "Svalbard and Jan Mayen",
+        "Sweden",
+        "Switzerland",
+        "Syrian Arab Republic",
+        "Taiwan",
+        "Tajikistan",
+        "Tanzania, United Republic of",
+        "Thailand",
+        "Timor-Leste",
+        "Togo",
+        "Tokelau",
+        "Tonga",
+        "Trinidad and Tobago",
+        "Tunisia",
+        "Turkey",
+        "Turkmenistan",
+        "Turks and Caicos Islands",
+        "Tuvalu",
+        "Uganda",
+        "Ukraine",
+        "United Arab Emirates",
+        "United Kingdom",
+        "United States",
+        "Uruguay",
+        "Uzbekistan",
+        "Vanuatu",
+        "Venezuela",
+        "Vietnam",
+        "Virgin Islands",
+        "Virgin Islands",
+        "Wallis and Futuna",
+        "Western Sahara",
+        "Yemen",
+        "Zambia",
+        "Zimbabwe"
+      ],
+      selected_city: 'default',
+      cityList: Array(),
+      isDesktop: !this.$q.platform.is.mobile
     }
   },
   methods: {
+    async onCountryChanged(v){
+      // Reset city list
+      this.cityList = Array()
+
+      // Fetch cities
+      let cities = await ApiService.getcities(v)
+
+      // Map cities
+      this.mapCities(cities.data.data)
+    },
+    mapCities(cities){
+      if(this.cityList == null || this.cityList == undefined) return;
+      this.cityList = cities
+      this.selected_city = cities[0]
+    },
+    async onCityChanged(v){
+      console.log(this.selected_country)
+      console.log(this.selected_city)
+    }
 
   },
   mounted () {
@@ -58,5 +322,10 @@ export default {
 <style scoped>
     .input-spacer-padding{
         padding: 5px;
+    }
+
+    .activeDesktop{
+        max-width: 700px !important;
+        width: 700px !important;
     }
 </style>
