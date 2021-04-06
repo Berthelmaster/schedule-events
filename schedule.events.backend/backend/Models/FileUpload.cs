ce899f608c15b2601ec24d8bd00cdf2eb321eb91
Berthelmaster <berthel0802@gmail.com>
2021-04-06 21:47:49 +0200

    file upload added model

---
+++ schedule.events.backend/backend/Models/FileUpload.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Models
+{
+    public class FileUpload
+    {
+        public int Id { get; set; }
+        public string FileName { get; set; }
+        public string FullFilePath { get; set; }
+        public string Owner { get; set; }
+        public string FileType { get; set; }
+        public int Size { get; set; }
+        public bool IsImage { get; set; }
+        public DateTime UploadDate { get; set; }
+        public DateTime? Expires { get; set; } = null;
+
+    }
+}
