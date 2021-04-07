c08f16e8b81b598617a8ac754c948e64c3f6e7d0
Berthelmaster <berthel0802@gmail.com>
2021-04-07 10:06:25 +0200

    frontpage

--- schedule.events.frontend/src/pages/Frontpage.vue
+++ schedule.events.frontend/src/pages/Frontpage.vue
@@ -36,11 +36,50 @@
         </div>
       </div>
       <div class="content-events">
-        <ul>
-          <li v-for="event in posts" :key="event.id">
-            {{event}}
-          </li>
-        </ul>
+    <q-card class="my-card" v-for="event in posts" :key="event.id">
+      <q-img src="https://cdn.quasar.dev/img/chicken-salad.jpg" />
+
+      <q-card-section>
+        <q-btn
+          fab
+          color="primary"
+          icon="place"
+          class="absolute"
+          style="top: 0; right: 12px; transform: translateY(-50%);"
+        />
+
+        <div class="row no-wrap items-center">
+          <div class="col text-h6 ellipsis">
+            {{event.title}}
+          </div>
+          <div class="col-auto text-grey text-caption q-pt-md row no-wrap items-center">
+            <q-icon name="place" />
+            {{event.city}} - 
+            {{event.country}}
+          </div>
+        </div>
+
+        <q-rating v-model="stars" :max="5" size="32px" />
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <div class="text-subtitle1">
+          $・{{event.description}}
+        </div>
+        <div class="text-caption text-grey">
+          {{event.content}}
+        </div>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-actions>
+        <q-btn flat round icon="event" />
+        <q-btn flat color="primary">
+          Reserve
+        </q-btn>
+      </q-card-actions>
+    </q-card>
       </div>
       <div>
         <q-btn class="btn-full-width absolute-bottom" @click="$router.push('/create-event')" color="blue" outlined label="Create event" />
@@ -311,7 +350,8 @@ export default {
       ],
       selected_city: 'default',
       cityList: Array(),
-      posts: []
+      posts: [],
+      stars: 4
     }
   },
   methods: {
@@ -367,4 +407,10 @@ export default {
   width: 100%;
   margin-bottom: 5px;
 }
+.my-card{
+  width: 100%;
+  max-width: 45rem;
+  margin: auto;
+  margin-top: 2rem;
+  }
 </style>
