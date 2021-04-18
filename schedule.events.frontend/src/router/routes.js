54b87a66defe5c26eb66c19c215c946cc3a05f15
Berthelmaster <berthel0802@gmail.com>
2021-04-18 09:22:12 +0200

    updated pages frontend

--- schedule.events.frontend/src/router/routes.js
+++ schedule.events.frontend/src/router/routes.js
@@ -5,9 +5,11 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('src/pages/Frontpage.vue') },
-      { path: '/login', component: () => import('src/pages/Login.vue')},
+      { path: 'login', component: () => import('src/pages/Login.vue')},
       { path: 'register', component: () => import('src/pages/Register.vue')},
-      { path: 'create-event', component: () => import('src/pages/CreateEvent.vue')}
+      { path: 'create-event', component: () => import('src/pages/CreateEvent.vue')},
+      { path: 'profile', component: () => import('src/pages/Profile.vue')},
+      { path: 'about-and-donate', component: () => import('src/pages/About.vue')}
     ],
   },
   {
