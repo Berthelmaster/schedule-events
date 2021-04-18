6ea6e0e3500fadab4a767d85468a089150dceced
Berthelmaster <berthel0802@gmail.com>
2021-04-18 20:14:05 +0200

    added profile

--- schedule.events.frontend/src/pages/Profile.vue
+++ schedule.events.frontend/src/pages/Profile.vue
@@ -1,8 +1,55 @@
 <template>
     <q-page class="flex-center q-pa-sm bg-grey-3">
+      <q-field filled label="Email" stack-label>
+        <template v-slot:control>
+          <div class="self-center full-width no-outline" tabindex="0">{{profile.email}}</div>
+        </template>
+      </q-field>
+      <q-field filled label="Full name" stack-label>
+        <template v-slot:control>
+          <div class="self-center full-width no-outline" tabindex="1">{{profile.fullName}}</div>
+        </template>
+      </q-field>
+      <q-field filled label="Birthday" stack-label>
+        <template v-slot:control>
+          <div class="self-center full-width no-outline" tabindex="2">{{profile.birthday}}</div>
+        </template>
+      </q-field>
+      <q-field filled label="Is Approved" stack-label>
+        <template v-slot:control>
+          <div class="self-center full-width no-outline" tabindex="3">{{profile.isApproved}}</div>
+        </template>
+      </q-field>
+      <q-field filled label="My role" stack-label>
+        <template v-slot:control>
+          <div class="self-center full-width no-outline" tabindex="4">{{profile.role}}</div>
+        </template>
+      </q-field>
     </q-page>
 </template>
 
 <script>
+import ApiService from '../services/api.service';
 
+export default {
+    data() {
+        return{
+            profile: []
+        }
+    },
+    methods: {
+        async getProfile(){
+            await ApiService.getProfile()
+            .then(res => {
+                this.profile = res.result.data
+            })
+            .catch(rej => {
+                this.profile = null;
+            })
+        }
+    },
+    async created() {
+        await this.getProfile();
+    }
+}
 </script>
