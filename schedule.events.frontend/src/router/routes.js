9d2aa4fddf594af71a4e6f05edbf65deb28d9822
Berthelmaster <berthel0802@gmail.com>
2021-04-20 14:19:24 +0200

    try stop lazy load

--- schedule.events.frontend/src/router/routes.js
+++ schedule.events.frontend/src/router/routes.js
@@ -1,3 +1,4 @@
+import Register from '../pages/Register'
 
 const routes = [
   {
@@ -6,7 +7,7 @@ const routes = [
     children: [
       { name: 'frontpage', path: '', component: () => import('src/pages/Frontpage.vue')},
       { name: 'login', path: 'login', component: () => import('src/pages/Login.vue')},
-      { name: 'register',path: 'register', component: () => import('src/pages/Register.vue')},
+      { name: 'register',path: 'register', component: Register},
       { name: 'create-event', path: 'create-event', component: () => import('src/pages/CreateEvent.vue')},
       { name: 'profile', path: 'profile', component: () => import('src/pages/Profile.vue')},
       { name: 'about-and-donate', path: 'about-and-donate', component: () => import('src/pages/About.vue')}
