433f1d0f98ca49b46843eb85b753576b07785694
Berthelmaster <berthel0802@gmail.com>
2021-04-22 19:09:16 +0200

    view updated

--- schedule.events.frontend/src/pages/View-event.vue
+++ schedule.events.frontend/src/pages/View-event.vue
@@ -1,6 +1,40 @@
 <template>
     <q-page class="flex-center q-pa-sm bg-grey-3" v-if="getEvent">
         {{post}}
+        <div class="text-h3">
+            <section class="center-it">{{post.title}}</section>
+        </div>
+
+        <q-separator />
+        
+        <div>
+            <section>
+                <q-img v-if="post.image != ''" :src="post.image" 
+                style="max-width: 100%; height: 30rem; width: 100%;"
+                contain/>
+                <q-img v-else src="https://api.linkancestors.com/download?websitePath=4/27e888de-dd14-4c41-9196-1ab340338101no-image.png" 
+                style="max-width: 100%; height: 30rem; width: 100%;"
+                contain/>
+            </section>
+        </div>
+
+        <q-separator />
+
+        <div>
+            <section>
+                <div class="text-h5"><b>Description: </b> {{post.description}}</div>
+                <div class="text-h5"><b>Content: </b>{{post.content}}</div>
+            </section>
+        </div>
+
+        <q-separator />
+
+        <div>
+            <section>
+                <div id="comments" class="text-h4">0 Comments</div>
+                <q-editor v-model="editor" min-height="5rem" />
+            </section>
+        </div>
     </q-page>
 </template>
 
@@ -16,7 +50,8 @@ export default {
           id: this.$router.currentRoute.query.id,
           post: {
 
-          }
+          },
+          editor: "I like this event..."
       }
   },
 
@@ -39,3 +74,13 @@ export default {
 
 // this.$router.currentRoute.query.id
 </script>
+
+
+<style scoped>
+.center-it{
+  margin: auto;
+  width: 50%;
+  text-align: center;
+  margin-bottom: 1rem;
+}
+</style>
