c83c065a2c5f08d5504e480a60ff091968e1d381
Berthelmaster <Berthel0802@gmail.com>
2021-04-28 11:42:49 +0200

    cleaned up JobBase

--- schedule.events.backend/backend/Extensions/Hangfire/Background.Tasks/Jobs/JobBase.cs
+++ schedule.events.backend/backend/Extensions/Hangfire/Background.Tasks/Jobs/JobBase.cs
@@ -4,11 +4,11 @@ namespace backend.Extensions.Hangfire.Background.Tasks.Jobs
 {
     public abstract class JobBase
     {
-        protected readonly AppDbContext _context;
+        protected readonly AppDbContext Context;
 
         protected JobBase(AppDbContext context)
         {
-            _context = context;
+            Context = context;
         }
 
         public abstract void AddHangfireJob();
